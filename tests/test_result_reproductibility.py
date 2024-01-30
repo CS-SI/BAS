ccd7c2a254454fca603ad59f871292a8286fc0b2
Emery Charlotte <charlotte.emery@c-s.fr>
2024-01-30 17:45:41 +0100

    [test] add simple unitary-like tests

---
+++ tests/test_result_reproductibility.py
@@ -0,0 +1,71 @@
+import os
+
+os.environ['USE_PYGEOS'] = '0'
+from argparse import ArgumentParser
+import geopandas as gpd
+import tarfile
+from matplotlib import pyplot as plt
+
+tar_gold_results = "widths_examples_gold.tar.gz"
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Parse and process data from ICESat-2"
+    )
+
+    parser.add_argument(
+        "-id_test", dest="id_test", default=None,
+        help="Path to granule directory with ICESat-2 ATL13 data from direct download or restart csv file")
+    parser.add_argument(
+        "-output_run", dest="output_run", default=None,
+        help="Path to shp files with width outputs to tests")
+
+    # Read and check optionnal input arguments
+    opt = parser.parse_args()
+    if opt.id_test is None:
+        l_id_test = [1, 2, 3, 4, 5, 6]
+
+    else:
+        l_id_test = [int(i) for i in opt.id_test.split(",")]
+    print(l_id_test)
+    if opt.output_run is None:
+        raise ValueError("Missing widths file to validated")
+    else:
+        l_output_run = [s for s in opt.output_run.split(",")]
+        if len(l_output_run) != len(l_id_test):
+            raise ValueError("Number of input file different from number of input test id")
+
+    # Loop over tests to realize
+    for id_test in l_id_test:
+
+        print("Check test {} ...".format(id_test))
+
+        # Untar gold simulations
+        str_gold = "widths_example{}.shp".format(id_test)
+        tar = tarfile.open(tar_gold_results)
+        for ext in ["shp", "shx", "prj", "dbf", "cpg"]:
+            tar.extract("widths_example{}.{}".format(id_test, ext))
+        tar.close()
+
+        # Load gold width
+        gdf_gold = gpd.read_file(str_gold)
+        print(gdf_gold)
+
+        # Load test width
+        gdf_test = gpd.read_file(l_output_run[id_test-1])
+        print(gdf_test)
+
+        # merge width
+        gdf_test.rename(mapper={"width": "width_ref"}, axis=1, inplace=True)
+        gdf_test["width_gold"] = gdf_gold["width"]
+        gdf_test["width_diff"] = gdf_test["width_ref"] - gdf_test["width_gold"]
+
+        gdf_test.plot.scatter(x="width_ref", y="width_gold", c="width_diff", colormap="viridis")
+        print("Correlation : {}".format(gdf_test["width_gold"].corr(gdf_gold["width"])))
+        print(gdf_test["width_diff"].describe())
+
+        if abs(gdf_test["width_diff"].mean()) > 0.1:
+            print("Test failed ...")
+        else:
+            print("Test passed ...")
+        plt.show()
---
+++ tests/test_unit.py
@@ -0,0 +1,129 @@
+import os
+
+os.environ['USE_PYGEOS'] = '0'
+import geopandas as gpd
+
+from basprocessor import WidthProcessing
+
+# Inputs for test_1
+str_watermask_1_tif = "unit_watermask_1.tif"
+str_reaches_1_shp = "unit_centerline_1.shp"
+str_sections_1_shp = "unit_sections_1.shp"
+
+# Inputs for test_2
+str_watermask_2_tif = "unit_watermask_2.tif"
+str_reaches_2_shp = "unit_centerline_2.shp"
+str_sections_2_shp = "unit_sections_2.shp"
+
+# Inputs for test_3
+str_watermask_3_tif = "unit_watermask_3.tif"
+str_reaches_3_shp = "unit_centerline_3.shp"
+str_sections_3_shp = "unit_sections_3.shp"
+
+
+def test_1():
+    # Load sections and reaches
+    gdf_reaches = gpd.read_file(str_reaches_1_shp)
+    gdf_sections = gpd.read_file(str_sections_1_shp)
+
+    # Set config #1
+    dct_cfg_test1 = {"clean": {"bool_clean": False,
+                               "type_label": "base",
+                               "fpath_wrkdir": ".",
+                               "gdf_waterbodies": None
+                               },
+                     "label": {"bool_label": False,
+                               "type_label": "base",
+                               "fpath_wrkdir": "."
+                               },
+                     "widths": {"scenario": 0
+                                }
+                     }
+
+    # Instanciate basprocessor
+    processor = WidthProcessing(
+        str_watermask_tif=str_watermask_1_tif,
+        gdf_sections=gdf_sections,
+        gdf_reaches=gdf_reaches,
+        attr_reachid="id",
+        str_proj="proj"
+    )
+    processor.preprocessing()
+    gdf_widths = processor.processing(dct_cfg_test1)
+    print(gdf_widths)
+
+
+def test_2():
+    # Load sections and reaches
+    gdf_reaches = gpd.read_file(str_reaches_2_shp)
+    gdf_sections = gpd.read_file(str_sections_2_shp)
+
+    # Set config #2
+    dct_cfg_test2 = {"clean": {"bool_clean": False,
+                               "type_label": "base",
+                               "fpath_wrkdir": ".",
+                               "gdf_waterbodies": None
+                               },
+                     "label": {"bool_label": False,
+                               "type_label": "base",
+                               "fpath_wrkdir": "."
+                               },
+                     "widths": {"scenario": 0
+                                }
+                     }
+
+    # Instanciate basprocessor
+    processor = WidthProcessing(
+        str_watermask_tif=str_watermask_2_tif,
+        gdf_sections=gdf_sections,
+        gdf_reaches=gdf_reaches,
+        attr_reachid="id",
+        str_proj="proj"
+    )
+    processor.preprocessing()
+    gdf_widths = processor.processing(dct_cfg_test2)
+    print(gdf_widths)
+
+
+def test_3():
+    # Load sections and reaches
+    gdf_reaches = gpd.read_file(str_reaches_3_shp)
+    gdf_sections = gpd.read_file(str_sections_3_shp)
+
+    # Set config #3
+    dct_cfg_test3 = {"clean": {"bool_clean": False,
+                               "type_label": "base",
+                               "fpath_wrkdir": ".",
+                               "gdf_waterbodies": None
+                               },
+                     "label": {"bool_label": False,
+                               "type_label": "base",
+                               "fpath_wrkdir": "."
+                               },
+                     "widths": {"scenario": 0
+                                }
+                     }
+
+    # Instanciate basprocessor
+    processor = WidthProcessing(
+        str_watermask_tif=str_watermask_3_tif,
+        gdf_sections=gdf_sections,
+        gdf_reaches=gdf_reaches,
+        attr_reachid="id",
+        str_proj="proj"
+    )
+    processor.preprocessing()
+    gdf_widths = processor.processing(dct_cfg_test3)
+    print(gdf_widths)
+
+
+if __name__ == "__main__":
+
+    # Test 1
+    test_1()
+
+    # Test 2
+    test_2()
+
+    # Test 3
+    test_3()
