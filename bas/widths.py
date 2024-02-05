b2e623b73bd37ac3be0956f4c84fab884f7c6025
Emery Charlotte <charlotte.emery@c-s.fr>
2024-02-05 13:33:32 +0100

    [refac] add header to src scripts

--- bas/widths.py
+++ bas/widths.py
@@ -1,9 +1,27 @@
+# Copyright (C) 2023-2024 CS GROUP France, https://csgroup.eu
+#
+# This file is part of BAS (Buffer Around Sections)
+#
+#     https://github.com/CS-SI/BAS
+#
+# Authors:
+#     Charlotte Emery, Kévin Larnier
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 module widths.py
 : Contains functions to compute widths from a list of cross-sections over a watermask
-Created 2021
-Latest modified on Jan 10, 2024
-Authors: Kevin Larnier, Charlotte Emery
 """
 import os
 import geopandas as gpd
